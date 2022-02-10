9169ef996670b9f93d7079d652849c4cd2c4b936
Jakub Švarc <jakubsvarc@protonmail.com>
2022-02-10 06:42:03 +0100

    feat: added connection status

---
+++ src/srsc/ConnectionStatus.java
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+
+package srsc;
+
+/**
+ *
+ * @author atlas144
+ */
+public class ConnectionStatus {
+    
+    private boolean connected = false;
+
+    public boolean isConnected() {
+        return connected;
+    }
+
+    public void setConnected(boolean connected) {
+        this.connected = connected;
+    }
+    
+}
