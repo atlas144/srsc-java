d6d7b8536bc845b8d8b5f053223fa0fd6ff2cc98
Jakub Švarc <jakubsvarc@protonmail.com>
2022-03-14 00:33:32 +0100

    test: added PacketProcessorTest

--- src/test/java/srsc/PacketProcessorTest.java
+++ src/test/java/srsc/PacketProcessorTest.java
@@ -2,11 +2,10 @@
 
 package srsc;
 
-import java.util.Arrays;
 import java.util.HashMap;
-import srsc.exceptions.MissingPayloadException;
+import org.junit.Assert;
+import org.junit.Test;
 import srsc.exceptions.PayloadParsingException;
-import srsc.packet.Packet;
 import srsc.packet.PacketType;
 import srsc.packet.PayloadSize;
 
@@ -14,48 +13,164 @@ import srsc.packet.PayloadSize;
  *
  * @author atlas144
  */
-public class PacketProcessor {
+public class PacketProcessorTest {
     
-    public static boolean validateChecksum(byte[] binaryPacket) {
-        byte validationSum = 0;
-
-        for (byte packetByte : binaryPacket) {
-            validationSum += packetByte;
-        }
-
-        return validationSum == 255;
+    @Test
+    public void testConstructor() {
+        final PacketProcessor testPacketProcessor = new PacketProcessor();
+        
+        Assert.assertNotNull(testPacketProcessor);
     }
     
-    public static int parseBinaryPayload(byte[] binaryPayload) throws PayloadParsingException {
-        if (binaryPayload.length < 1) {
-            throw new PayloadParsingException();
+    @Test
+    public void testValidateChecksumSuccess() {
+        final byte[] testBinaryPacket = {
+            0x01,
+            0x00,
+            0x40,
+            0x00,
+            0x00,
+            0x00
+        };
+        
+        byte checksum = 0x00;
+        
+        for (byte packetByte : testBinaryPacket) {
+            checksum += Byte.toUnsignedInt(packetByte);
         }
         
-        int payload = binaryPayload[0];
-
-        for (byte i = 1; i < binaryPayload.length; i++) {
-            payload += binaryPayload[i] * (256 * i);
-        }
+        testBinaryPacket[1] = (byte) ~checksum;
         
-        return payload;
+        Assert.assertTrue(PacketProcessor.validateChecksum(testBinaryPacket));
     }
     
-    public static Packet buildPacketObject(byte[] binaryPacket, HashMap<Integer, PacketType> packetTypes) throws PayloadParsingException {
-        Packet packet = null;
-        PacketType packetType = packetTypes.get((int) binaryPacket[0]);
+    @Test
+    public void testValidateChecksumFail() {
+        final byte[] testBinaryPacket = {
+            0x01,
+            0x00,
+            0x40,
+            0x00,
+            0x00,
+            0x00
+        };
         
-        if (packetType.getPayloadSize() != PayloadSize.COMMAND) {
-            int payloadOffset = packetType.isCritical() ? 3 : 2;
-            int payload = parseBinaryPayload(Arrays.copyOfRange(binaryPacket, payloadOffset, payloadOffset + packetType.getPayloadSize().getValue()));
+        Assert.assertFalse(PacketProcessor.validateChecksum(testBinaryPacket));
+    }
+    
+    @Test
+    public void testParsePayload() {
+        final int testPayload = 0x01020304;
+        final byte[] testBinaryPayload = {
+            0x04,
+            0x03,
+            0x02,
+            0x01
+        };
+        
+        Assert.assertArrayEquals(testBinaryPayload, PacketProcessor.parsePayload(testPayload, PayloadSize.INT));
+    }
+    
+    @Test
+    public void testParseBinaryPayload() {
+        try {
+            final byte[] testBinaryPayload =  {
+                0x04,
+                0x03,
+                0x02,
+                0x01
+            };
+            final int testPayload = 0x01020304;
             
-            packet = new Packet(packetType, payload);
-        } else {
-            try {
-                packet = new Packet(packetType);
-            } catch (MissingPayloadException exception) {}
-        }
-        
-        return packet;
+            Assert.assertEquals(testPayload, PacketProcessor.parseBinaryPayload(testBinaryPayload));
+        } catch (PayloadParsingException ex) {}
+    }
+    
+    @Test
+    public void testParseBinaryPayloadMissingPayload() {
+        final byte[] testBinaryPayload = new byte[0];
+
+        Assert.assertThrows(PayloadParsingException.class, () -> {
+            PacketProcessor.parseBinaryPayload(testBinaryPayload);
+        });
+    }
+    
+    @Test
+    public void testBuildPacketObject() {
+        try {
+            final HashMap<Integer, PacketType> testPacketTypes = new HashMap<>();
+            final byte[] testBinaryPacket = {
+                0x01,
+                0x00,
+                0x40,
+                0x00,
+                0x00,
+                0x00
+            };
+            
+            testPacketTypes.put(0x01, new PacketType((byte) 0x01, PayloadSize.INT));
+
+            byte checksum = 0x00;
+            
+            for (byte packetByte : testBinaryPacket) {
+                checksum += Byte.toUnsignedInt(packetByte);
+            }
+            
+            testBinaryPacket[1] = (byte) ~checksum;
+            
+            Assert.assertNotNull(PacketProcessor.buildPacketObject(testBinaryPacket, testPacketTypes));
+        } catch (PayloadParsingException ex) {}
+    }
+    
+    @Test
+    public void testBuildPacketObjectCommand() {
+        try {
+            final HashMap<Integer, PacketType> testPacketTypes = new HashMap<>();
+            final byte[] testBinaryPacket = {
+                0x05,
+                0x00
+            };
+            
+            testPacketTypes.put(0x05, new PacketType((byte) 0x05, PayloadSize.COMMAND));
+                        
+            byte checksum = 0x00;
+            
+            for (byte packetByte : testBinaryPacket) {
+                checksum += Byte.toUnsignedInt(packetByte);
+            }
+            
+            testBinaryPacket[1] = (byte) ~checksum;
+            
+            Assert.assertNotNull(PacketProcessor.buildPacketObject(testBinaryPacket, testPacketTypes));
+        } catch (PayloadParsingException ex) {}
+    }
+    
+    @Test
+    public void testBuildPacketObjectCritical() {
+        try {
+            final HashMap<Integer, PacketType> testPacketTypes = new HashMap<>();
+            final byte[] testBinaryPacket = {
+                0x06,
+                0x00,
+                0x40,
+                0x40,
+                0x00,
+                0x00,
+                0x00
+            };
+            
+            testPacketTypes.put(0x06, new PacketType((byte) 0x06, PayloadSize.INT, true));
+            
+            byte checksum = 0x00;
+            
+            for (byte packetByte : testBinaryPacket) {
+                checksum += Byte.toUnsignedInt(packetByte);
+            }
+            
+            testBinaryPacket[1] = (byte) ~checksum;
+            
+            Assert.assertNotNull(PacketProcessor.buildPacketObject(testBinaryPacket, testPacketTypes));
+        } catch (PayloadParsingException ex) {}
     }
 
 }
